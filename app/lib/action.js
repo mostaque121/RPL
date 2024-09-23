9b51d282a8610243de72f1e9dea7f5c4a5f8d05d
mostaque121 <162879301+mostaque121@users.noreply.github.com>
2024-09-23 14:45:56 +0600

    Update action.js

--- app/lib/action.js
+++ app/lib/action.js
@@ -1,6 +1,11 @@
 'use server'
-import { revalidatePath } from "next/cache"
+import { revalidatePath, revalidateTag } from "next/cache"
 export async function revalidateService() {
     console.log('revalidating')
     revalidatePath('/services')
+};
+
+export async function revalidateServiceTag() {
+    console.log('revalidating')
+    revalidateTag('service')
 }
